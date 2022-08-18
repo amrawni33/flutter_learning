4823e0b011f458350c933739cd200c9476f9caac
Amr Awni AbdelTawab Youssef <83365437+amrawni33@users.noreply.github.com>
2022-08-18 02:17:33 +0200

    sec 7

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -5,6 +5,9 @@ class NewsLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:startup_namer/modules/counter/counterscreen.dart';
 import 'package:startup_namer/modules/login/login_screen.dart';
 import 'package:startup_namer/shared/bloc_observer.dart';
 
+import 'layout/news_app/news_layout.dart';
 import 'layout/todo_app/todo_layout.dart';
 
 void main() {
@@ -17,8 +19,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+         backgroundColor: Colors.white,
+          elevation: 0.0,
+          backwardsCompatibility: false,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.white,
+            statusBarIconBrightness: Brightness.dark
+          )
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: HomeLayout(),
+      home: NewsLayout(),
     );
   }
 }
